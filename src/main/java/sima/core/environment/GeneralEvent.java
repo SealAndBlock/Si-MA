e457c1d678a032ce2ca87b061997cf4afc37d81c
Guillaume <guil.rako@hotmail.fr>
2020-09-22 11:44:29 +0200

    #24 update documentation

--- src/main/java/sima/core/environment/GeneralEvent.java
+++ src/main/java/sima/core/environment/GeneralEvent.java
@@ -2,10 +2,19 @@ package sima.core.environment;
 
 import sima.core.agent.AbstractAgent;
 
+/**
+ * Class which represents an {@link Event} with no protocol targeted.
+ */
 public class GeneralEvent extends Event {
 
     // Constructors.
 
+    /**
+     * Constructs a {@link GeneralEvent}. A GeneralEvent is an {@link Event} with no protocol targeted.
+     *
+     * @param sender   the agent sender
+     * @param receiver the agent receiver
+     */
     public GeneralEvent(AbstractAgent sender, AbstractAgent receiver) {
         super(sender, receiver, null);
     }
