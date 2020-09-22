7c7aa6c916cd31b219eae617419093c910b821f2
Guillaume <guil.rako@hotmail.fr>
2020-09-22 11:33:28 +0200

    #24 Removing of the method receivedMessage in Environment

--- src/main/java/sima/core/environment/Environment.java
+++ src/main/java/sima/core/environment/Environment.java
@@ -10,9 +10,9 @@ import java.util.List;
  * layer of the communication. An environment determine if two agents can communicate or not.
  * <p>
  * For example, you can create an environment where agent are mobil and move. Each agent has a maximum scope and can
- * communicate only with agent which are in this scope. Therefore in methods {@link #sendMessage(Message)}
- * *and {@link #receiveMessage(Message)}  you can verify if the {@link Message#getSender()}} and the
- * {@link Message#getReceiver()} are in the range to send and receive message.
+ * communicate only with agent which are in this scope. Therefore, in method {@link #sendMessage(Message)} you can
+ * verify if the {@link Message#getSender()}} and the {@link Message#getReceiver()} are in the range to send and
+ * receive a message.
  *
  * @author guilr
  */
@@ -58,16 +58,6 @@ public interface Environment {
      * @param message the message to send
      */
     void sendMessage(Message message);
-
-    /**
-     * Received the message. Look the {@link Message#getProtocolTargeted()} and call the method
-     * {@link Protocol#processEvent(Event)} }.
-     * <p>
-     * In the method you can also verify if the message can be received or not.
-     *
-     * @param message the message received
-     */
-    void receiveMessage(Message message);
 
     /**
      * @return the unique name of the environment, cannot be null.
