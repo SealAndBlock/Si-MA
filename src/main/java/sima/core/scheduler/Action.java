0204fe759087e79c8cefc81ff96d34ae0a805f96
Guillaume <guil.rako@hotmail.fr>
2020-10-14 22:56:49 +0200

    #57 Create classes Scheduler and Action

--- src/main/java/sima/core/scheduler/Action.java
+++ src/main/java/sima/core/scheduler/Action.java
@@ -1,18 +1,24 @@
 package sima.core.scheduler;
 
-import sima.core.agent.AbstractAgent;
-
-import java.util.UUID;
+import sima.core.agent.AgentIdentifier;
 
 public abstract class Action {
 
     // Variables.
 
-    private final UUID executorAgent;
+    /**
+     * The agent which executes the action.
+     * <p>
+     * An {@code Action} is not necessarily execute by an agent.
+     * <p>
+     * When an {@code Action} is executed by an {@link sima.core.agent.AbstractAgent}, it is because the method called
+     * in the method {@link #execute()} is a method of the executor agent.
+     */
+    private final AgentIdentifier executorAgent;
 
     // Constructors.
 
-    public Action(UUID executorAgent) {
+    public Action(AgentIdentifier executorAgent) {
         this.executorAgent = executorAgent;
     }
 
@@ -23,4 +29,9 @@ public abstract class Action {
      */
     public abstract void execute();
 
+    // Getters and Setters.
+
+    public AgentIdentifier getExecutorAgent() {
+        return executorAgent;
+    }
 }
--- src/main/java/sima/core/scheduler/Scheduler.java
+++ src/main/java/sima/core/scheduler/Scheduler.java
@@ -1,4 +1,30 @@
 package sima.core.scheduler;
 
+import sima.core.environment.event.Event;
+
 public interface Scheduler {
+
+    /**
+     * Schedule the execution of the {@link Action}. In other words, schedule the moment when the method
+     * {@link Action#execute()} is called and execute. The waiting time is the number of time unit that the scheduler
+     * must wait after the call of this method to execute the {@code Action}.
+     *
+     * @param action      the action to schedule
+     * @param waitingTime the time to wait before execute the action
+     * @throws IllegalArgumentException if the waitingTime is less than 0.
+     */
+    void scheduleAction(Action action, long waitingTime);
+
+    /**
+     * Schedule the to send of the {@link Event}. In other words, schedule the moment when the method
+     * {@link sima.core.agent.AbstractAgent#processEvent(Event)} of the {@link Event#getReceiver()} agent is called.
+     * Therefore, the specified {@code Event} must have a not null receiver, else throws an
+     * {@link NullPointerException}.
+     *
+     * @param event       the event to schedule
+     * @param waitingTime the time to wait before send the event
+     * @throws IllegalArgumentException if the waitingTime is less than 0.
+     * @throws NullPointerException     if the {@link Event#getReceiver()} is null
+     */
+    void scheduleEvent(Event event, long waitingTime);
 }
