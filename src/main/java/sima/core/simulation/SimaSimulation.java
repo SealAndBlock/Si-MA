0a279be5dbfce0930e74360d7ec5bbb00f0e628a
Guillaume <guil.rako@hotmail.fr>
2020-10-20 16:53:58 +0200

    #70 Update the default method Scheduler#scheduleEvent
    - create the singleton SimaSimulation#SIMA_SIMULATION
    - add the private method SimaSimulation#findAgent
    - add the static method SimaSimulation#getAgentFromIdentifier
    - update the default implementation of Scheduler#scheduleEvent

--- src/main/java/sima/core/simulation/SimaSimulation.java
+++ src/main/java/sima/core/simulation/SimaSimulation.java
@@ -1,8 +1,62 @@
 package sima.core.simulation;
 
+import sima.core.agent.AbstractAgent;
+import sima.core.agent.AgentIdentifier;
+import sima.core.simulation.exception.TwoAgentWithSameIdentifierException;
+
+import java.util.List;
+
 public class SimaSimulation {
-    
+
+    // Singleton.
+
+    private static final SimaSimulation SIMA_SIMULATION = new SimaSimulation();
+
     // Variables
-  
+
     private AgentManager agentManager;
+
+    // Constructors.
+
+    // Methods.
+
+    /**
+     * Finds in the {@link #agentManager} the agent which as the same {@link AgentIdentifier} than the specified agent
+     * identifier.
+     *
+     * @param agentIdentifier the identifier of the wanted agent
+     * @return the agent associate to the identifier, returns null if the agent is not found.
+     * @throws NullPointerException                if the agentIdentifier is null.
+     * @throws TwoAgentWithSameIdentifierException if there is in the agent manager two agents with the same identifier
+     */
+    public static AbstractAgent getAgentFromIdentifier(AgentIdentifier agentIdentifier) {
+        return SIMA_SIMULATION.findAgent(agentIdentifier);
+    }
+
+    /**
+     * Finds in the {@link #agentManager} the agent which as the same {@link AgentIdentifier} than the specified agent
+     * identifier.
+     *
+     * @param agentIdentifier the identifier of the wanted agent
+     * @return the agent associate to the identifier, returns null if the agent is not found.
+     * @throws NullPointerException                if the agentIdentifier is null.
+     * @throws TwoAgentWithSameIdentifierException if there is in the agent manager two agents with the same identifier
+     */
+    private AbstractAgent findAgent(AgentIdentifier agentIdentifier) {
+        if (agentIdentifier == null)
+            throw new NullPointerException("The agent identifier cannot be null.");
+
+        List<AbstractAgent> agents = this.agentManager.getAllAgents();
+        AbstractAgent res = null;
+        for (AbstractAgent agent : agents) {
+            if (agent.getAgentIdentifier().equals(agentIdentifier)) {
+                if (res == null)
+                    res = agent;
+                else
+                    throw new TwoAgentWithSameIdentifierException("Agent1 = " + res + " Agent2 = " + agent);
+            }
+        }
+
+        return res;
+    }
 }
