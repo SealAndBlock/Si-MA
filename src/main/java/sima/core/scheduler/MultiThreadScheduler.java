b0cec2dac61838761165808f914b19fca332176f
Guillaume <guil.rako@hotmail.fr>
2020-11-04 14:57:12 +0100

    #86 Create the abstract class MultiThreadScheduler

--- src/main/java/sima/core/scheduler/MultiThreadScheduler.java
+++ src/main/java/sima/core/scheduler/MultiThreadScheduler.java
@@ -1,45 +1,97 @@
 package sima.core.scheduler;
 
-public class MultiThreadScheduler implements Scheduler{
+import java.util.List;
+import java.util.Vector;
+
+public abstract class MultiThreadScheduler implements Scheduler {
 
     // Variables.
 
+    /**
+     * True if the {@link Scheduler} is started, else false.
+     */
+    protected boolean isStarted = false;
+
+    /**
+     * The end of the simulation.
+     */
+    private final long endSimulation;
+
+    /**
+     * The number of thread use to execute all {@link Executable}.
+     */
+    private final int nbExecutorThread;
+
+    /**
+     * The list of all {@link sima.core.scheduler.Scheduler.SchedulerWatcher}.
+     */
+    private final List<SchedulerWatcher> schedulerWatchers;
+
+    private final List<ExecutorThread> executorThreadList;
+
     // Constructors.
+
+    public MultiThreadScheduler(long endSimulation, int nbExecutorThread) {
+        this.endSimulation = endSimulation;
+        if (this.endSimulation < 1)
+            throw new IllegalArgumentException("The end simulation time must be greater or equal to 1.");
+
+        this.nbExecutorThread = nbExecutorThread;
+        if (this.nbExecutorThread < 1)
+            throw new IllegalArgumentException("The number of executor thread must be greater or equal to 1.");
+
+        this.schedulerWatchers = new Vector<>();
+
+        this.executorThreadList = new Vector<>();
+    }
 
     // Methods.
 
     @Override
-    public boolean addSchedulerWatcher(SchedulerWatcher schedulerWatcher) {
-        return false;
+    public synchronized boolean addSchedulerWatcher(SchedulerWatcher schedulerWatcher) {
+        if (this.schedulerWatchers.contains(schedulerWatcher))
+            return false;
+
+        return this.schedulerWatchers.add(schedulerWatcher);
     }
 
     @Override
     public void removeSchedulerWatcher(SchedulerWatcher schedulerWatcher) {
-
+        this.schedulerWatchers.remove(schedulerWatcher);
     }
 
-    @Override
-    public boolean start() {
-        return false;
+    private void updateSchedulerWatcherOnSchedulerStarted() {
+        this.schedulerWatchers.forEach(SchedulerWatcher::schedulerStarted);
     }
 
-    @Override
-    public boolean kill() {
-        return false;
+    private void updateSchedulerWatcherOnSchedulerKilled() {
+        this.schedulerWatchers.forEach(SchedulerWatcher::schedulerKilled);
     }
 
-    @Override
-    public void scheduleExecutable(Executable executable, long waitingTime, ScheduleMode scheduleMode, long nbRepetitions, long executionTimeStep) {
-
+    private void updateSchedulerWatcherOnSimulationEndTimeReach() {
+        this.schedulerWatchers.forEach(SchedulerWatcher::simulationEndTimeReach);
     }
 
-    @Override
-    public void scheduleExecutableAtSpecificTime(Executable executable, long simulationSpecificTime) {
-
+    private void updateSchedulerWatcherOnNoExecutableToExecute() {
+        this.schedulerWatchers.forEach(SchedulerWatcher::noExecutableToExecute);
     }
 
-    @Override
-    public long getCurrentTime() {
-        return 0;
+    // Inner classes.
+
+    protected abstract class ExecutorThread implements Runnable {
+
+        // Variables.
+
+        protected boolean isFinished = false;
+
+        // Getters and Setters.
+
+        public boolean isFinished() {
+            return isFinished;
+        }
+
+        public void setFinished(boolean finished) {
+            isFinished = finished;
+        }
     }
 }
