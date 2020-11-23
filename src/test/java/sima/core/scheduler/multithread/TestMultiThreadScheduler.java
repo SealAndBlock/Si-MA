3e09abfcb5447f711389e5094ebcda79862121ed
Guillaume Rakotomalala <guil.rako@hotmail.fr>
2020-11-23 01:47:10 +0100

    #99 Implementing TestScheduler
    - Add tests for scheduleRepeated

--- src/test/java/sima/core/scheduler/multithread/TestMultiThreadScheduler.java
+++ src/test/java/sima/core/scheduler/multithread/TestMultiThreadScheduler.java
@@ -1,7 +1,10 @@
 package sima.core.scheduler.multithread;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import sima.core.scheduler.TestScheduler;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Disabled
 public abstract class TestMultiThreadScheduler extends TestScheduler {
@@ -10,4 +13,13 @@ public abstract class TestMultiThreadScheduler extends TestScheduler {
 
     protected int NB_EXECUTOR_THREADS = 5;
 
+    // Setup.
+
+    @BeforeEach
+    @Override
+    public void setup() {
+        super.setup();
+
+        assertTrue(NB_EXECUTOR_THREADS > 0, "NB_EXECUTOR_THREADS cannot be less or equal to 0");
+    }
 }
