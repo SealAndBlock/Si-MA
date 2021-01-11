b3e9a0a754bb7d10e9024631c9d59c58a7e3a332
Guillaume Rakotomalala <guil.rako@hotmail.fr>
2021-01-11 17:18:52 +0100

    #117 Create SimaSimulationTest

---
+++ src/test/java/sima/core/simulation/SimaSimulationTest.java
@@ -0,0 +1,14 @@
+package sima.core.simulation;
+
+import sima.core.SimaTest;
+
+public class SimaSimulationTest extends SimaTest {
+
+    // Initialisation.
+
+    @Override
+    protected void verifyAndSetup() {
+    }
+
+    // Tests.
+}
